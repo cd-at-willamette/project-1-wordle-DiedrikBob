e8176df2abb5a55b979f73f18e9a43372f00a8a7
DiedrikBob <dcboberg@willamette.edu>
2024-09-29 17:40:44 -0700

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -11,8 +11,6 @@ from english import * # ENGLISH_WORDS, is_english_word
 import random
 
 
-
-
 def wordle():
     
     def get_word_from_row():
@@ -20,20 +18,22 @@ def wordle():
         word_from_row = ""
         for i in range(5):
             word_from_row += gw.get_square_letter(row, i)
-        #gw.show_message(word_from_row)
+
         word_from_row = word_from_row.lower()
         return word_from_row
+
 
 
     def contains_repeated_letters(word):
         for index in range(len(word)): #loop through every letter of word
             for letter in range(index + 1, len(word)):
-                #print(word[index], word[letter]) #test to see overlapping letters --> repeating
                 if word[index] == word[letter]: #if letter is equal then the letter is repeating
                     return True
                 
         else: 
             return False
+
+
 
     def random_five_letter_word():
         random.shuffle(ENGLISH_WORDS)
@@ -44,24 +44,25 @@ def wordle():
                     return str_word
 
 
-    def word_to_row(word:str, row:int):
-        for i in range(5):    
-            gw.set_square_letter(row, i, word[i])
-
 
     def compare_word():
         row = gw.get_current_row()
+        my_word = get_word_from_row()
 
         for index in range(len(wordle_word)):
             
-            if get_word_from_row()[index] in wordle_word and get_word_from_row()[index] == wordle_word[index]:
+            if my_word[index] in wordle_word and my_word[index] == wordle_word[index]:
                 gw.set_square_color(row, index, "green")
-
-            elif get_word_from_row()[index] in wordle_word:
-                gw.set_square_color(row, index, PRESENT_COLOR) 
-
-            else:
-                gw.set_square_color(row, index, "grey")
+                my_word = my_word.replace(my_word[index], "-")
+                print(my_word, index)
+        for index in range(len(wordle_word)):
+            if gw.get_square_color(row,index) !=  "green":
+                if my_word[index] in wordle_word and my_word[index] != wordle_word[index]:
+                        gw.set_square_color(row, index, PRESENT_COLOR)
+                        my_word = my_word.replace(my_word[index], "-")
+                        print(my_word, index)
+                else:
+                    gw.set_square_color(row, index, "black")
 
     
     def check_green_letters():
@@ -80,10 +81,9 @@ def wordle():
             return False
 
     def check_affiration():
-        
-        if get_word_from_row() in ENGLISH_WORDS:
-            #gw.show_message("Yes, its a word.")
+        if get_word_from_row() in ENGLISH_WORDS and len(get_word_from_row()) == 5:
             return True
+        
         else:
             gw.show_message("It's not a word.")
             return False
@@ -93,18 +93,34 @@ def wordle():
         if gw.get_current_row() != 5 and check_win() == False:
                 gw.set_current_row(row + 1)
             
-                print(row)
         elif gw.get_current_row() == 5 and check_win() == False:
             gw.show_message("""Sry buddy, the game is over. But YOU'RE AWESOME!""") # Try again. Never give up! You can make this!""")
             
+    
+    def color_keys():
+        for i in range(len(wordle_word)):
+            if get_word_from_row()[i] == wordle_word[i]:
+                gw.get_key_color(get_word_from_row()[i])
+                gw.set_key_color(get_word_from_row()[i], "green")
+
+            elif get_word_from_row()[i] in wordle_word:
+                if gw.get_key_color(get_word_from_row()[i]) != "green":
+                    gw.set_key_color(get_word_from_row()[i], PRESENT_COLOR)
+            
+            else:
+                gw.get_key_color(get_word_from_row()[i])
+                gw.set_key_color(get_word_from_row()[i], "black")
 
             #gw.set_current_row()
+
+    
 
     def enter_action():
         
         if check_affiration() == True:
             compare_word()
             check_win()
+            color_keys()
             change_row()
         
             
@@ -118,7 +134,7 @@ def wordle():
     gw.add_enter_listener(enter_action)
     #gw.add_enter_listener(compare_word)
 
-    
+
 
     #word_to_row("Hello",0)
 
